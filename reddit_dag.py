454c8bde1a038ef8c3a463699c274d70ba2cddaf
Param Jaswal <paramjaswal@Params-MacBook-Pro.local>
2025-04-28 12:01:40 -0700

    chore: add Docker creds and CI/CD secrets

--- reddit_dag.py
+++ reddit_dag.py
@@ -1,32 +1,28 @@
-from datetime import timedelta
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from airflow import DAG
 from airflow.operators.python import PythonOperator
-import pendulum
+from datetime import datetime
 from reddit_ETL import run_ETL
 
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': pendulum.now().subtract(days=1),
-    'email': ['parambytes@gmail.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'start_date': datetime(2025, 4, 27),
     'retries': 1,
-    'retry_delay': timedelta(minutes=1)
 }
 
-dag = DAG(
-    'reddit_dag',
+with DAG(
+    dag_id='reddit_dag',
     default_args=default_args,
-    description='Basic ETL practice',
-    schedule_interval=None,
-    catchup=False
-)
-
-run_etl_task = PythonOperator(
-    task_id='complete_reddit_etl',
-    python_callable=run_ETL,
-    dag=dag
-)
-
-run_etl_task
+    schedule='@daily',
+    catchup=False,
+    tags=['reddit'],
+) as dag:
+    
+    reddit_etl_task = PythonOperator(
+        task_id='run_reddit_etl',
+        python_callable=run_ETL,
+    )
